00436032f1e23988d4d29b390d15bcb8e165993e
Benjamin Evan Cowles <becowles93@gmail.com>
2024-07-19 11:22:19 -0400

    chore: Implement routes and stub components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
-  return <div>App</div>;
-};
+import { Box } from "@mui/material";
+
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from "./components";
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
